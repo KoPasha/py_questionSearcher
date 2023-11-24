7a36d2571b1e20a6b9b8f7967d4f440fcf14e14f
KoPasha <kpp123@gmail.com>
2023-11-24 16:28:27 +0300

    json files and qt designer version

--- questionSearcherUIC.py
+++ questionSearcherUIC.py
@@ -1,8 +1,21 @@
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
+from PyQt5 import QtCore, uic
+
+class SearcherGUI(QMainWindow):
+
+    def __init__(self):
+        super(SearcherGUI, self).__init__()
+        uic.loadUi('searcherMainWindow.ui', self)
+        self.show()
+        self.searchAgainButton.clicked.connect(self.search_button_clicked)
+
+    def search_button_clicked(self):
+        pass
 
 def main():
-    pass
+    app = QApplication([])
+    window =  SearcherGUI()
+    app.exec_()
 
 if __name__ == '__main__':
     main()
